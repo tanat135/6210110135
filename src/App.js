4cb20ef14f8ea7eb3c3d117c31b159c0b9d5bf2e
tanat165 <6210110135@psu.ac.th>
2021-07-05 11:19:59 +0700

    part4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import React from 'react';
 import './App.css';
+import WordCard from './WordCard';
 
+const word = "Hello"
 function App() {
   return (
     <div>
-      Hello world
+     <WordCard value={word}/>
+      
     </div>
   );
 }
